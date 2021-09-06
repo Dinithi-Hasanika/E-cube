6c25737f9b390c419946b53d8a4fbd187fb60404
Dinithi Hasanika <dinithiliyanage.95@gmail.com>
2021-09-06 11:42:01 +0530

    Customize

---
+++ e-cube/src/Components/Dashboard/Customize.js
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import "./Users.css";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Roles from "../Roles/Roles";
+import Permissions from "../Permissions/Permissions";
+
+class Customize extends Component {
+    render() {
+        return (
+            <div>
+                <Router basename="/">
+                    <div className="whole">
+                        <div className="linkside">
+                            <div className="gap">
+                                <Link
+                                    to="/roles"
+                                    className="btn btn-outline-success my-2 my-sm-0"
+                                >
+                                    ROLES
+                                </Link>
+                            </div>
+                            <div className="gap">
+                                <Link
+                                    to="/permissions"
+                                    className="btn btn-outline-success my-2 my-sm-0"
+                                >
+                                    PERMISSIONS
+                                </Link>
+                            </div>
+
+                        </div>
+                        <div className="barside">
+                            <Route
+                                exact
+                                path="/roles"
+                                component={Roles}
+                            />
+                            <Route exact path="/permissions" component={Permissions} />
+                        </div>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default Customize;
